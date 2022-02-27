5be3421b8c2d2c1740e23e47c296a4411c3686d5
Julie <jq.nikitina@gmail.com>
2022-02-28 00:03:56 +0700

    Какой-то прогресс

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import './Header.css';
 
 import logoPath from '../../images/header/logo.svg';
 import profileLogoPath from '../../images/header/account.svg';
+import menuLogoPath from '../../images/header/menu.svg';
 
 import React from "react";
 import {Link} from "react-router-dom";
@@ -11,10 +12,11 @@ function Header(props) {
   return (
     <header className="header">
       <img className="header__logo" src={logoPath} alt="Лого"/>
-      <div className="header__links">
-        {props.isLoggedIn
-          ?
-          <>
+
+      {props.isLoggedIn
+        ?
+        <>
+          <div className="header__links">
             <Link to="/movies" className="header__link"> Фильмы </Link>
             <Link to="/saved-movies" className="header__link"> Сохраненные фильмы </Link>
             <Link to="/profile" className="header__profile-link">
@@ -23,15 +25,19 @@ function Header(props) {
                 <span>Аккаунт</span>
               </button>
             </Link>
-          </>
-          :
-          <>
-            <Link to="/sign-up" className="header__link"> Регистрация </Link>
-            <button className="header__auth-button"> Войти </button>
-          </>
-        }
-      </div>
-
+          </div>
+          <Link to="/profile" className="header__menu-button">
+            <button className="header__menu-button">
+              <img className="header__menu-logo" src={menuLogoPath}/>
+            </button>
+          </Link>
+        </>
+        :
+        <div className="header__creds">
+          <Link to="/sign-up" className="header__link"> Регистрация </Link>
+          <button className="header__auth-button"> Войти</button>
+        </div>
+      }
     </header>
   );
 }
