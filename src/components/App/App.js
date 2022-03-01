60f0d8420f6143fd65091c97d51d201cf8c7ef3e
Julie <jq.nikitina@gmail.com>
2022-03-02 00:26:18 +0700

    404

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,6 +10,7 @@ import {CurrentUserContext} from "../../contexts/CurrentUserContext";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Main from "../Main/Main";
 import Movies from "../Movies/Movies";
+import NotFound from "../NotFound/NotFound";
 
 
 function App() {
@@ -22,14 +23,14 @@ function App() {
     <CurrentUserContext.Provider value={currentUser}>
       <div className="page">
         <BrowserRouter>
-          <Header isLoggedIn={loggedIn}/>
-          <Routes>
-            <Route path="/" element={<Main/>}/>
-            <Route path="/movies" element={<Movies/>}/>
-          </Routes>
+          {/*<Header isLoggedIn={loggedIn}/>*/}
+          {/*<Routes>*/}
+          {/*  <Route path="/" element={<Main/>}/>*/}
+          {/*  <Route path="/movies" element={<Movies/>}/>*/}
+          {/*</Routes>*/}
+          <NotFound/>
         </BrowserRouter>
-        <Footer/>
-
+        {/*<Footer/>*/}
       </div>
     </CurrentUserContext.Provider>
 
